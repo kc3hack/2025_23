4104b2f67f97f21b8d7da69cdc62e60224bf20b6
darallium <contact@darallium.com>
2025-02-18 07:30:52 +0900

    fix chore

--- llm-next/llm_next/embeddings.py
+++ llm-next/llm_next/embeddings.py
@@ -2,15 +2,15 @@
 
 
 def init_embeddings(local: bool = False, model_name: str = "intfloat/multilingual-e5-large"):
-	"""Embeddingsモデルを初期化して返す"""
-	# Hugging Face Embeddingsを使う場合
-	if local:
-		from langchain.embeddings import HuggingFaceEmbeddings
+    """Embeddingsモデルを初期化して返す"""
+    # Hugging Face Embeddingsを使う場合
+    if local:
+        from langchain.embeddings import HuggingFaceEmbeddings
 
-		embeddings = HuggingFaceEmbeddings(model_name=model_name)
-	else:
-		# Google Generative AI Embeddingsを使う場合
-		from langchain_google_genai import GoogleGenerativeAIEmbeddings
+        embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    else:
+        # Google Generative AI Embeddingsを使う場合
+        from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-		embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-	return embeddings
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    return embeddings
