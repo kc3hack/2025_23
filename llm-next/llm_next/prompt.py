ee520841ece8ab1db0c95154f1105c67803fb114
darallium <contact@darallium.com>
2025-02-18 06:57:05 +0900

    モジュール分割済み

---
+++ llm-next/llm_next/prompt.py
@@ -0,0 +1,13 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+
+next_gen_prompt = ChatPromptTemplate.from_template(
+    """
+    以下のcontextだけに基づいて回答してください。
+    {context}
+    
+    質問: 
+    {question}
+    """
+)
+
--- llm-next/main.py
+++ llm-next/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from llm-db!")
-
+from llm_next.llm_next import main
+import os
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
